40e21ee1f169fc700911e35afc60f8f306b134c2
James Etchells <etch.rush@gmail.com>
2022-02-23 10:21:05 -0800

    Add navLinks element to nav bar Add links array and append each item to navLinks element

--- src/nav.js
+++ src/nav.js
@@ -1,12 +1,30 @@
 const nav = () => {
     const element = document.createElement('nav');
     const logo = document.createElement('span');
+    const navLinks = document.createElement('ul');
+    const button = document.createElement('button');
+    const links = ['Home', 'Menu', 'Hours', 'Contact']
 
     element.setAttribute('class', 'nav');
     logo.setAttribute('class', 'logo');
+    navLinks.setAttribute('id', 'nav-links');
+    button.setAttribute('type', 'button');
+    button.setAttribute('id', 'res-btn');
+
+
+    for (let i = 0; i < links.length; i++) {
+        const a = document.createElement('a');
+        a.innerHTML = links[i];
+        navLinks.appendChild(a);
+    }
+
+    navLinks.appendChild(button);
+
     logo.innerHTML = 'Cafe Sevilla';
+    button.innerHTML = 'Reservation';
 
     element.appendChild(logo);
+    element.appendChild(navLinks);
 
     return element;
 }
