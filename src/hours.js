9c62b7098a3c6155a526658deb43566b8efc57fd
James Etchells <etch.rush@gmail.com>
2022-02-24 11:36:26 -0800

    Add menu hours and contact js files, began work on hours script

---
+++ src/hours.js
@@ -0,0 +1,24 @@
+const hours = () => {
+    const element = document.createElement('section');
+    const title = document.createElement('h1');
+    const table = document.createElement('table');
+    const th = document.createElement('th');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+
+    const hours = {
+        Monday: 'closed',
+        Tuesday: '5pm - 10pm',
+        Wednesday: '5pm - 10pm',
+        Thursday: '5pm - 10pm',
+        Friday: '5pm - 10pm',
+        Saturday: '5pm - 10pm',
+        Sunday: '5pm - 10pm',
+        Monday: '5pm - 10pm',
+    }
+
+
+    return element;
+}
+
+export default hours;
